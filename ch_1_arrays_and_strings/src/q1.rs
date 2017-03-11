07af399368233796e0ad5a70d14b8727324f41de
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2017-03-12 00:59:18 +0530

    Fix q1_1

--- ch_1_arrays_and_strings/src/q1.rs
+++ ch_1_arrays_and_strings/src/q1.rs
@@ -1,9 +1,10 @@
 #[cfg(test)]
 fn is_unique_chars(input_string: &str) -> bool {
 	let mut char_vec: Vec<char> = input_string.chars().collect();
-	char_vec.dedup();
-	let unique_string: String = char_vec.into_iter().collect();
-	unique_string == input_string
+    char_vec.sort();
+	let sorted_char_vec: Vec<char> = char_vec.clone();
+    char_vec.dedup();
+	sorted_char_vec == char_vec
 }
 
 #[cfg(test)]
@@ -11,8 +12,8 @@ mod q1_tests {
 	use q1::is_unique_chars;
     #[test]
     #[should_panic]
-    fn hello_is_not_unique() {
-    	assert!(is_unique_chars("Hello"));
+    fn lol_is_not_unique() {
+    	assert!(is_unique_chars("lol"));
     }
 
     #[test]
