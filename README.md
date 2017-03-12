829c16faeabf908e9c73383238ad4e5d545945c9
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2017-03-12 19:45:58 +0530

    Added README

---
+++ README.md
@@ -0,0 +1,8 @@
+Rust CTCI
+===
+
+[Cracking the Coding Interview](http://www.crackingthecodinginterview.com/) solutions in [Rust](https://www.rust-lang.org/en-US/).
+
+## Contributing
+
+I am currently not accepting pull requests. This is a personal exercise to learn Rust and prepare for interviews. I might accept contributions later.
--- ch_1_arrays_and_strings/src/q1.rs
+++ ch_1_arrays_and_strings/src/q1.rs
@@ -3,11 +3,11 @@ use std::collections::HashMap;
 
 #[cfg(test)]
 fn is_unique_chars_sort(input_string: &str) -> bool {
-	let mut char_vec: Vec<char> = input_string.chars().collect();
+    let mut char_vec: Vec<char> = input_string.chars().collect();
     char_vec.sort();
-	let sorted_char_vec: Vec<char> = char_vec.clone();
+    let sorted_char_vec: Vec<char> = char_vec.clone();
     char_vec.dedup();
-	return sorted_char_vec == char_vec;
+    return sorted_char_vec == char_vec;
 }
 
 #[cfg(test)]
@@ -25,27 +25,27 @@ fn is_unique_chars_hash(input_string: &str) -> bool {
 
 #[cfg(test)]
 mod q1_tests {
-	use q1::is_unique_chars_sort;
+    use q1::is_unique_chars_sort;
 
     #[test]
     #[should_panic]
     fn lol_is_not_unique() {
-    	assert!(is_unique_chars_sort("lol"));
+        assert!(is_unique_chars_sort("lol"));
     }
 
     #[test]
     fn world_is_unique() {
-    	assert!(is_unique_chars_sort("World"));
+        assert!(is_unique_chars_sort("World"));
     }
 
-	#[test]
+    #[test]
     fn empty_is_unique() {
-    	assert!(is_unique_chars_sort(""));
+        assert!(is_unique_chars_sort(""));
     }
 
     #[test]
     fn a_is_unique() {
-    	assert!(is_unique_chars_sort("a"));
+        assert!(is_unique_chars_sort("a"));
     }
 
     use q1::is_unique_chars_hash;
