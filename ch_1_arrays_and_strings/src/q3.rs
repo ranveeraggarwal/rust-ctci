f56f3ab0cb8f56f40da6292f88425d3a60699893
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2017-03-13 21:29:53 +0530

    q1_3 alternate

--- ch_1_arrays_and_strings/src/q3.rs
+++ ch_1_arrays_and_strings/src/q3.rs
@@ -19,8 +19,38 @@ fn urlify (input_string: &str, true_length:i32) -> String {
 }
 
 #[cfg(test)]
+// This is the methid described in the book.
+fn urlify_reverse(input_string: &str, true_length:i32) -> String {
+    let mut space_count: i32 = 0;
+    for (i, c) in input_string.chars().enumerate() {
+        if i == true_length as usize {
+            break;
+        }
+        if c == ' ' {
+            space_count += 1;
+        }
+    }
+    let mut runner: usize = true_length as usize + (space_count as usize * 2);
+    let mut char_vec: Vec<char> = input_string.chars().collect();
+    for i in (0..true_length).rev() {
+        if char_vec[i as usize] == ' ' {
+            char_vec[runner-1] = '0';
+            char_vec[runner-2] = '2';
+            char_vec[runner-3] = '%';
+            runner -= 3;
+        } else {
+            char_vec[runner-1] = char_vec[i as usize];
+            runner -= 1;
+        }
+    }
+    let return_string: String = char_vec.into_iter().collect(); 
+    return return_string;
+}
+
+#[cfg(test)]
 mod q3_tests {
     use q3::urlify;
+    use q3::urlify_reverse;
 
     #[test]
     fn urlify_test_1() {
@@ -41,5 +71,26 @@ mod q3_tests {
     #[should_panic]
     fn urlify_test_4() {
         assert_eq!(urlify(" Mr John Smith     ", 14), "%20Mr%20John%20Smith");
-    }    
+    }
+
+    #[test]
+    fn urlify_reverse_test_1() {
+        assert_eq!(urlify_reverse("Mr John Smith    ", 13), "Mr%20John%20Smith");
+    }
+
+    #[test]
+    fn urlify_reverse_test_2() {
+        assert_eq!(urlify_reverse("Mr John Smith       ", 14), "Mr%20John%20Smith%20");
+    }
+
+    #[test]
+    fn urlify_reverse_test_3() {
+        assert_eq!(urlify_reverse(" Mr John Smith      ", 14), "%20Mr%20John%20Smith");
+    }
+
+    #[test]
+    #[should_panic]
+    fn urlify_reverse_test_4() {
+        assert_eq!(urlify_reverse(" Mr John Smith     ", 14), "%20Mr%20John%20Smith");
+    }        
 }
