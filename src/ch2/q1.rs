23235f3d050a67fec6184182d0cfe3b4ea525fec
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2017-03-25 00:45:40 +0530

    q2_1

--- src/ch2/q1.rs
+++ src/ch2/q1.rs
@@ -4,7 +4,8 @@ use std::collections::HashSet;
 use std::collections::LinkedList;
 
 #[cfg(test)]
-fn remove_duplicates_bad(list: &LinkedList<u32>) -> LinkedList<u32> {
+// This is O(n), but uses O(n) memory.
+fn remove_duplicates(list: &LinkedList<u32>) -> LinkedList<u32> {
 	let mut set = HashSet::new();
 	let mut return_list = LinkedList::new();
 
@@ -14,17 +15,16 @@ fn remove_duplicates_bad(list: &LinkedList<u32>) -> LinkedList<u32> {
 		} 
 		set.insert(node);
 	}
-
 	return return_list;
 }
 
 #[cfg(test)]
 mod q1_tests {
-	use ch2::q1::remove_duplicates_bad;
+	use ch2::q1::remove_duplicates;
 	use std::collections::LinkedList;
 
 	#[test]
-	fn remove_duplicates_bad_random() {
+	fn remove_duplicates_random() {
 		let mut list_1 = LinkedList::new();
 		let mut list_2 = LinkedList::new();
 		list_1.push_back(1);
@@ -34,22 +34,22 @@ mod q1_tests {
 		list_2.push_back(1);
 		list_2.push_back(2);
 		list_2.push_back(3);
-		let unique_list_1 = remove_duplicates_bad(&list_1);
+		let unique_list_1 = remove_duplicates(&list_1);
 		assert_eq!(list_2, unique_list_1);
 	}
 
 	#[test]
-	fn remove_duplicates_bad_singly() {
+	fn remove_duplicates_singly() {
 		let mut list_1 = LinkedList::new();
 		let mut list_2 = LinkedList::new();
 		list_1.push_back(1);
 		list_2.push_back(1);
-		let unique_list_1 = remove_duplicates_bad(&list_1);
+		let unique_list_1 = remove_duplicates(&list_1);
 		assert_eq!(list_2, unique_list_1);
 	}
 
 	#[test]
-	fn remove_duplicates_bad_cascading() {
+	fn remove_duplicates_cascading() {
 		let mut list_1 = LinkedList::new();
 		let mut list_2 = LinkedList::new();
 		list_1.push_back(1);
@@ -57,7 +57,7 @@ mod q1_tests {
 		list_1.push_back(1);
 		list_1.push_back(1);
 		list_2.push_back(1);
-		let unique_list_1 = remove_duplicates_bad(&list_1);
+		let unique_list_1 = remove_duplicates(&list_1);
 		assert_eq!(list_2, unique_list_1);
 	}
 }
